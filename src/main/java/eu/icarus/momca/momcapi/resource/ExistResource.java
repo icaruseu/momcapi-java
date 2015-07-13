4ed4b94919331fbb73bf4193501fe933423a05b9
Daniel <djeller@nettek.at>
2015-07-13 14:00:37 +0200

    Javadoc

--- src/main/java/eu/icarus/momca/momcapi/resource/ExistResource.java
+++ src/main/java/eu/icarus/momca/momcapi/resource/ExistResource.java
@@ -11,7 +11,10 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * Created by daniel on 24.06.2015.
+ * An XML resource stored in the database used by MOM-CA.
+ *
+ * @author Daniel Jeller
+ *         Created on 24.06.2015.
  */
 public class ExistResource {
 
@@ -22,12 +25,25 @@ public class ExistResource {
     @NotNull
     private final Document xmlAsDocument;
 
+
+    /**
+     * Instantiates a new ExistResource with an existing resource.
+     *
+     * @param existResource The ExistResource to use.
+     */
     ExistResource(@NotNull final ExistResource existResource) {
         this.resourceName = existResource.getResourceName();
         this.xmlAsDocument = existResource.getXmlAsDocument();
         this.parentUri = existResource.getParentUri();
     }
 
+    /**
+     * Instantiates a new ExistResource.
+     *
+     * @param resourceName        The name of the resource, e.g. {@code user.xml}.
+     * @param parentCollectionUri The URI of the collection, the resource is stored in in the database, e.g. {@code /db/mom-data/xrx.user}.
+     * @param xmlContent          The xml content of the resource as {@code String}.
+     */
     public ExistResource(@NotNull final String resourceName, @NotNull final String parentCollectionUri, @NotNull final String xmlContent) {
 
         try {
@@ -44,6 +60,12 @@ public class ExistResource {
 
     }
 
+    /**
+     * Query the resource's XML content.
+     *
+     * @param query The Xpath Query to execute on the content.
+     * @return The nodes containing the results.
+     */
     @NotNull
     final Nodes listQueryResultNodes(@NotNull XpathQuery query) {
 
@@ -54,6 +76,12 @@ public class ExistResource {
 
     }
 
+    /**
+     * The Xpath Query to execute on the content.
+     *
+     * @param query the query
+     * @return A list of the results as strings.
+     */
     @NotNull
     final List<String> listQueryResultStrings(@NotNull XpathQuery query) {
 
@@ -66,21 +94,33 @@ public class ExistResource {
 
     }
 
+    /**
+     * @return The URI of the collection, the resource is stored in in the database, e.g. {@code /db/mom-data/xrx.user}.
+     */
     @NotNull
     public String getParentUri() {
         return parentUri;
     }
 
+    /**
+     * @return The name of the resource in the database, e.g. {@code user.xml}
+     */
     @NotNull
     public String getResourceName() {
         return resourceName;
     }
 
+    /**
+     * @return The URI of the resource in the database, e.g. {@code /db/mom-data/xrx.user/user.xml}.
+     */
     @NotNull
     public String getUri() {
         return parentUri + "/" + resourceName;
     }
 
+    /**
+     * @return The XML as an XML document.
+     */
     @NotNull
     public Document getXmlAsDocument() {
         return xmlAsDocument;
