2824d2c0f651ace58e1ccac65c92badabf331c36
Daniel Jeller <djeller@nettek.at>
2015-08-07 12:20:16 +0200

    move XpathQueryTest to the correct class.

--- src/test/java/eu/icarus/momca/momcapi/query/XpathQueryTest.java
+++ src/test/java/eu/icarus/momca/momcapi/query/XpathQueryTest.java
@@ -1,11 +1,13 @@
-package eu.icarus.momca.momcapi.resource;
+package eu.icarus.momca.momcapi.query;
 
-import eu.icarus.momca.momcapi.query.XpathQuery;
+import eu.icarus.momca.momcapi.resource.MomcaResource;
 import nu.xom.Nodes;
 import org.jetbrains.annotations.NotNull;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,42 +33,42 @@ public class XpathQueryTest {
 
     @Test
     public void testQUERY_ATOM_EMAIL() throws Exception {
-        List<String> result = resource.queryContentAsList(XpathQuery.QUERY_ATOM_EMAIL);
+        List<String> result = queryContentAsList(resource, XpathQuery.QUERY_ATOM_EMAIL);
         assertEquals(result.size(), 1);
         assertEquals(result.get(0), "atomemail");
     }
 
     @Test
     public void testQUERY_ATOM_ID() throws Exception {
-        List<String> result = resource.queryContentAsList(XpathQuery.QUERY_ATOM_ID);
+        List<String> result = queryContentAsList(resource, XpathQuery.QUERY_ATOM_ID);
         assertEquals(result.size(), 1);
         assertEquals(result.get(0), "atomid");
     }
 
     @Test
     public void testQUERY_CEI_BODY_IDNO_TEXT() throws Exception {
-        List<String> result = resource.queryContentAsList(XpathQuery.QUERY_CEI_BODY_IDNO_TEXT);
+        List<String> result = queryContentAsList(resource, XpathQuery.QUERY_CEI_BODY_IDNO_TEXT);
         assertEquals(result.size(), 1);
         assertEquals(result.get(0), "idnotext");
     }
 
     @Test
     public void testQUERY_CEI_ISSUED() throws Exception {
-        List<String> result = resource.queryContentAsList(XpathQuery.QUERY_CEI_ISSUED);
+        List<String> result = queryContentAsList(resource, XpathQuery.QUERY_CEI_ISSUED);
         assertEquals(result.size(), 1);
         assertEquals(result.get(0), "ceiissued");
     }
 
     @Test
     public void testQUERY_CEI_TEXT() throws Exception {
-        Nodes result = resource.queryContentAsNodes(XpathQuery.QUERY_CEI_TEXT);
+        Nodes result = queryContentAsNodes(resource, XpathQuery.QUERY_CEI_TEXT);
         assertEquals(result.size(), 1);
     }
 
     @Test
     public void testQUERY_CEI_WITNESS_ORIG_FIGURE() throws Exception {
 
-        Nodes result = resource.queryContentAsNodes(XpathQuery.QUERY_CEI_WITNESS_ORIG_FIGURE);
+        Nodes result = queryContentAsNodes(resource, XpathQuery.QUERY_CEI_WITNESS_ORIG_FIGURE);
         assertEquals(result.size(), 2);
         assertEquals(result.get(0).toXML(), "<cei:figure n=\"nvalue1\"> <cei:graphic url=\"urlvalue1\">textvalue1</cei:graphic> </cei:figure>");
         assertEquals(result.get(1).toXML(), "<cei:figure> <cei:graphic url=\"urlvalue1\" /> </cei:figure>");
@@ -75,14 +77,14 @@ public class XpathQueryTest {
 
     @Test
     public void testQUERY_CEI__BODY_IDNO_ID() throws Exception {
-        List<String> result = resource.queryContentAsList(XpathQuery.QUERY_CEI_BODY_IDNO_ID);
+        List<String> result = queryContentAsList(resource, XpathQuery.QUERY_CEI_BODY_IDNO_ID);
         assertEquals(result.size(), 1);
         assertEquals(result.get(0), "idnoid");
     }
 
     @Test
     public void testQUERY_CONFIG_GROUP_NAME() throws Exception {
-        List<String> result = resource.queryContentAsList(XpathQuery.QUERY_CONFIG_GROUP_NAME);
+        List<String> result = queryContentAsList(resource, XpathQuery.QUERY_CONFIG_GROUP_NAME);
         List<String> expectedResult = new ArrayList<>(2);
         expectedResult.add("atom");
         expectedResult.add("guest");
@@ -91,50 +93,71 @@ public class XpathQueryTest {
 
     @Test
     public void testQUERY_CONFIG_NAME() throws Exception {
-        List<String> result = resource.queryContentAsList(XpathQuery.QUERY_CONFIG_NAME);
+        List<String> result = queryContentAsList(resource, XpathQuery.QUERY_CONFIG_NAME);
         String expectedResult = "configname";
         assertEquals(result.get(0), expectedResult);
     }
 
     @Test
     public void testQUERY_NAME() throws Exception {
-        List<String> result = resource.queryContentAsList(XpathQuery.QUERY_NAME);
+        List<String> result = queryContentAsList(resource, XpathQuery.QUERY_NAME);
         assertEquals(result.size(), 1);
         assertEquals(result.get(0), "name");
     }
 
     @Test
     public void testQUERY_XRX_BOOKMARK() throws Exception {
-        List<String> result = resource.queryContentAsList(XpathQuery.QUERY_XRX_BOOKMARK);
+        List<String> result = queryContentAsList(resource, XpathQuery.QUERY_XRX_BOOKMARK);
         assertEquals(result.size(), 1);
         assertEquals(result.get(0), "xrxbookmark");
     }
 
     @Test
     public void testQUERY_XRX_EMAIL() throws Exception {
-        List<String> result = resource.queryContentAsList(XpathQuery.QUERY_XRX_EMAIL);
+        List<String> result = queryContentAsList(resource, XpathQuery.QUERY_XRX_EMAIL);
         assertEquals(result.size(), 1);
         assertEquals(result.get(0), "xrxemail");
     }
 
     @Test
     public void testQUERY_XRX_MODERATOR() throws Exception {
-        List<String> result = resource.queryContentAsList(XpathQuery.QUERY_XRX_MODERATOR);
+        List<String> result = queryContentAsList(resource, XpathQuery.QUERY_XRX_MODERATOR);
         assertEquals(result.size(), 1);
         assertEquals(result.get(0), "xrxmoderator");
     }
 
     @Test
     public void testQUERY_XRX_NAME() throws Exception {
-        List<String> result = resource.queryContentAsList(XpathQuery.QUERY_XRX_NAME);
+        List<String> result = queryContentAsList(resource, XpathQuery.QUERY_XRX_NAME);
         assertEquals(result.size(), 1);
         assertEquals(result.get(0), "xrxname");
     }
 
     @Test
     public void testQUERY_XRX_SAVED_ID() throws Exception {
-        List<String> result = resource.queryContentAsList(XpathQuery.QUERY_XRX_SAVED_ID);
+        List<String> result = queryContentAsList(resource, XpathQuery.QUERY_XRX_SAVED_ID);
         assertEquals(result.size(), 1);
         assertEquals(result.get(0), "xrxsaved");
     }
+
+    private List<String> queryContentAsList(@NotNull MomcaResource resource, @NotNull XpathQuery query)
+            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+
+        Method queryMethod = MomcaResource.class.
+                getDeclaredMethod("queryContentAsList", XpathQuery.class);
+        queryMethod.setAccessible(true);
+        return (List<String>) queryMethod.invoke(resource, query);
+
+    }
+
+    private Nodes queryContentAsNodes(@NotNull MomcaResource resource, @NotNull XpathQuery query)
+            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+
+        Method queryMethod = MomcaResource.class.
+                getDeclaredMethod("queryContentAsNodes", XpathQuery.class);
+        queryMethod.setAccessible(true);
+        return (Nodes) queryMethod.invoke(resource, query);
+
+    }
+
 }
