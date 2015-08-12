44048b1313fde9fb20fc10a2390c3163c33f7390
Daniel Jeller <djeller@nettek.at>
2015-08-12 12:36:47 +0200

    MAPI-26 Implement HierarchyManager.listCollections()

--- src/test/java/eu/icarus/momca/momcapi/CollectionManagerTest.java
+++ src/test/java/eu/icarus/momca/momcapi/CollectionManagerTest.java
@@ -1,25 +1,47 @@
 package eu.icarus.momca.momcapi;
 
+import eu.icarus.momca.momcapi.resource.Country;
+import eu.icarus.momca.momcapi.resource.CountryCode;
+import eu.icarus.momca.momcapi.resource.Region;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.stream.Collectors;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
 /**
  * Created by djell on 11/08/2015.
  */
 public class CollectionManagerTest {
 
+    private CollectionManager cm;
+    private MomcaConnection mc;
+
+    @BeforeClass
+    public void setUp() throws Exception {
+        mc = TestUtils.initMomcaConnection();
+        cm = mc.getCollectionManager();
+        assertNotNull(cm, "MOM-CA connection not initialized.");
+    }
+
     @Test
     public void testListCollections() throws Exception {
-
+        assertEquals(cm.listCollections().size(), 2);
     }
 
     @Test
-    public void testListCollections1() throws Exception {
-
+    public void testListCollectionsForCountry() throws Exception {
+        Country country = mc.getCountryManager().getCountry(new CountryCode("BG")).get();
+        assertEquals(cm.listCollections(country).size(), 1);
     }
 
     @Test
-    public void testListCollections2() throws Exception {
-
+    public void testListCollectionsForRegion() throws Exception {
+        Country country = mc.getCountryManager().getCountry(new CountryCode("DE")).get();
+        Region region = country.getRegions().stream().filter(r -> r.getNativeName().equals("Bayern")).collect(Collectors.toList()).get(0);
+        assertEquals(cm.listCollections(region).size(), 1);
     }
 
 }
