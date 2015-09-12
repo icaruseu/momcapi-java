e930a341a634037296df2c36deaa0ba7fbeab66f
Daniel Jeller <djeller@nettek.at>
2015-09-12 14:05:06 +0200

    MAPI-24: Fully implement Charter
    Added the `@old` attribute as member field to `cei.Idno` class

--- src/main/java/eu/icarus/momca/momcapi/model/xml/cei/Idno.java
+++ src/main/java/eu/icarus/momca/momcapi/model/xml/cei/Idno.java
@@ -5,6 +5,8 @@ import nu.xom.Attribute;
 import nu.xom.Element;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Optional;
+
 /**
  * Represents a {@code cei:idno}-Element as defined in the
  * <a href="http://www.cei.lmu.de/element.php?ID=19">CEI-Schema</a>.<br/> This information represents the concept of a
@@ -21,8 +23,14 @@ public class Idno extends Element {
     @NotNull
     private final String id;
     @NotNull
+    private final Optional<String> old;
+    @NotNull
     private final String text;
 
+
+    public Idno(@NotNull String id, @NotNull String text) {
+        this(id, text, "");
+    }
 
     /**
      * Instantiates a new Idno.
@@ -30,14 +38,22 @@ public class Idno extends Element {
      * @param id   The {@code cei:idno/@id}-attribute.
      * @param text The text content.
      */
-    public Idno(@NotNull String id, @NotNull String text) {
+    public Idno(@NotNull String id, @NotNull String text, @NotNull String old) {
 
         super("cei:idno", Namespace.CEI.getUri());
+
         addAttribute(new Attribute("id", id));
         appendChild(text);
 
         this.text = text;
         this.id = id;
+
+        if (!old.isEmpty()) {
+            addAttribute(new Attribute("old", old));
+            this.old = Optional.of(old);
+        } else {
+            this.old = Optional.empty();
+        }
 
     }
 
@@ -47,6 +63,11 @@ public class Idno extends Element {
     @NotNull
     public String getId() {
         return id;
+    }
+
+    @NotNull
+    public Optional<String> getOld() {
+        return old;
     }
 
     /**
--- src/test/java/eu/icarus/momca/momcapi/model/xml/cei/IdnoTest.java
+++ src/test/java/eu/icarus/momca/momcapi/model/xml/cei/IdnoTest.java
@@ -2,28 +2,37 @@ package eu.icarus.momca.momcapi.model.xml.cei;
 
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.*;
 
 /**
  * Created by daniel on 09.07.2015.
  */
 public class IdnoTest {
 
-    private static final Idno CEI_IDNO = new Idno("id", "text");
+    public static final Idno CEI_IDNO_WITHOUT_OLD = new Idno("id", "text");
+    private static final Idno CEI_IDNO_WITH_OLD = new Idno("id", "text", "old");
 
     @Test
     public void testGetId() throws Exception {
-        assertEquals(CEI_IDNO.getId(), "id");
+        assertEquals(CEI_IDNO_WITH_OLD.getId(), "id");
     }
 
     @Test
     public void testGetText() throws Exception {
-        assertEquals(CEI_IDNO.getText(), "text");
+        assertEquals(CEI_IDNO_WITH_OLD.getText(), "text");
     }
 
     @Test
     public void testToXML() throws Exception {
-        assertEquals(CEI_IDNO.toXML(), "<cei:idno xmlns:cei=\"http://www.monasterium.net/NS/cei\" id=\"id\">text</cei:idno>");
+        assertEquals(CEI_IDNO_WITHOUT_OLD.toXML(), "<cei:idno xmlns:cei=\"http://www.monasterium.net/NS/cei\" id=\"id\">text</cei:idno>");
+        assertEquals(CEI_IDNO_WITH_OLD.toXML(), "<cei:idno xmlns:cei=\"http://www.monasterium.net/NS/cei\" id=\"id\" old=\"old\">text</cei:idno>");
+    }
+
+    @Test
+    public void testgetOld() throws Exception {
+        assertTrue(CEI_IDNO_WITH_OLD.getOld().isPresent());
+        assertEquals(CEI_IDNO_WITH_OLD.getOld().get(), "old");
+        assertFalse(CEI_IDNO_WITHOUT_OLD.getOld().isPresent());
     }
 
 }
