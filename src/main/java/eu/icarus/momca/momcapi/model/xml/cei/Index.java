fbf9ee46749076a85f09b2048782054a8177b6d5
Daniel Jeller <djeller@nettek.at>
2015-09-13 19:04:27 +0200

    MAPI-24: Added `cei.Index`

---
+++ src/main/java/eu/icarus/momca/momcapi/model/xml/cei/Index.java
@@ -0,0 +1,60 @@
+package eu.icarus.momca.momcapi.model.xml.cei;
+
+import nu.xom.Attribute;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+/**
+ * Created by djell on 13/09/2015.
+ */
+public class Index extends MixedContentElement {
+
+    @NotNull
+    private Optional<String> indexName = Optional.empty();
+    @NotNull
+    private Optional<String> lemma = Optional.empty();
+    @NotNull
+    private Optional<String> sublemma = Optional.empty();
+
+    Index(@NotNull String content, @NotNull String indexName, @NotNull String lemma, @NotNull String sublemma) {
+
+        super(content, "index");
+
+        if (!indexName.isEmpty()) {
+            addAttribute(new Attribute("indexName", indexName));
+            this.indexName = Optional.of(indexName);
+        }
+
+        if (!lemma.isEmpty()) {
+            addAttribute(new Attribute("lemma", lemma));
+            this.lemma = Optional.of(lemma);
+        }
+
+        if (!sublemma.isEmpty()) {
+            addAttribute(new Attribute("sublemma", sublemma));
+            this.sublemma = Optional.of(sublemma);
+        }
+
+    }
+
+    Index(@NotNull String content) {
+        this(content, "", "", "");
+    }
+
+    @NotNull
+    public Optional<String> getIndexName() {
+        return indexName;
+    }
+
+    @NotNull
+    public Optional<String> getLemma() {
+        return lemma;
+    }
+
+    @NotNull
+    public Optional<String> getSublemma() {
+        return sublemma;
+    }
+
+}
---
+++ src/test/java/eu/icarus/momca/momcapi/model/xml/cei/IndexTest.java
@@ -0,0 +1,41 @@
+package eu.icarus.momca.momcapi.model.xml.cei;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by djell on 13/09/2015.
+ */
+public class IndexTest {
+
+    @Test
+    public void test() throws Exception {
+
+        Index index = new Index("Urkunde");
+
+        assertEquals(index.getContent(), "Urkunde");
+        assertFalse(index.getIndexName().isPresent());
+        assertFalse(index.getLemma().isPresent());
+        assertFalse(index.getSublemma().isPresent());
+
+        String correctXml = "<index xmlns=\"http://www.monasterium.net/NS/cei\">Urkunde</index>";
+        assertEquals(index.toXML(), correctXml);
+
+        index = new Index("Waldi", "Tiere", "Hund", "Dackel");
+
+        assertEquals(index.getContent(), "Waldi");
+        assertTrue(index.getIndexName().isPresent());
+        assertEquals(index.getIndexName().get(), "Tiere");
+        assertTrue(index.getLemma().isPresent());
+        assertEquals(index.getLemma().get(), "Hund");
+        assertTrue(index.getSublemma().isPresent());
+        assertEquals(index.getSublemma().get(), "Dackel");
+
+
+        correctXml = "<index xmlns=\"http://www.monasterium.net/NS/cei\" indexName=\"Tiere\" lemma=\"Hund\" sublemma=\"Dackel\">Waldi</index>";
+        assertEquals(index.toXML(), correctXml);
+
+    }
+
+}
